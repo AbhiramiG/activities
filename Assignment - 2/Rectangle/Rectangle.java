48df5d3151e6c2da3523d2cf538911a91662b215
AbhiramiG <97896363+AbhiramiG@users.noreply.github.com>
2022-01-18 19:35:38 +0530

    Add files via upload

---
+++ Assignment - 2/Rectangle/Rectangle.java
@@ -0,0 +1,11 @@
+class Rectangle
+{
+public static void main (String args[])
+{
+int l=5,b=7,A,P;
+A=l+b;
+P=2*(l+b);
+System.out.println("Area of rectangle="+A);
+System.out.println("Perimeter of rectangular="+P);
+}
+}
---
+++ Assignment - 2/Square/Square.java
@@ -0,0 +1,12 @@
+class Square
+{
+public static void main(String args[])
+{
+int a=6;
+int square=(a*a);
+int cube=(a*a*a);
+int fourthpower=(a*a*a*a);
+System.out.println("squre="+square);
+System.out.println("fourthpower="+fourthpower);
+}
+}
