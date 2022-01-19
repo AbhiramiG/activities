4d936eb7f5c10a3b42f53838496b9fd556280963
AbhiramiG <97896363+AbhiramiG@users.noreply.github.com>
2022-01-19 23:02:39 +0530

    Add files via upload

---
+++ Assignment - 5/Gratestofthreenumbers/Gratestofthreenumbers.java
@@ -0,0 +1,18 @@
+class Gratestofthreenumbers
+{
+public static void main(String[] args)
+{
+int a=12;
+int b=15;
+int c=20;
+if(a>b)
+if(a>c)
+System.out.println("The greatest: " +a);
+if(b>a)
+if(b>c)
+System.out.println("The greatest: " +b);
+if(c>a)
+if(c>b)
+System.out.println(" The gratest: " +c);
+}
+}
---
+++ Assignment - 5/Naturalnumbers/Naturalnumbers.java
@@ -0,0 +1,13 @@
+class Naturalnumbers
+{
+public static void main (String[] args)
+{
+int i;
+System.out.println("The 10 Natural numbers are:\n");
+for(i=1;i<=10;i++)
+{
+System.out.println (i);
+}
+System.out.println ("\n");
+}
+}
