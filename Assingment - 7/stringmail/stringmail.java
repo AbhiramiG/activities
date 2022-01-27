1867c3175d3a606707fc354299b951e7d6d287bf
AbhiramiG <97896363+AbhiramiG@users.noreply.github.com>
2022-01-27 09:14:51 +0530

    Add files via upload

---
+++ Assingment - 7/stringmail/stringmail.java
@@ -0,0 +1,29 @@
+package Java_01;
+
+public class stringmail {
+
+	public static void main(String[] args) {
+
+    String m1= "abirami";
+	String m2= "jason";
+	String m3= "@";
+	String m4= "gmail";
+	String m5= ".";
+	String m6= "com";
+	
+	String m7= m1.concat(m2);
+	System.out.print(m7);
+	String m8 = m3.concat(m4);
+	System.out.print(m8);
+	String m9 = m5.concat(m6);
+	System.out.print(m9);      
+	
+	
+
+	
+	
+	
+	
+	}
+
+}
---
+++ Assingment - 7/stringreverse/png.strin.java
@@ -0,0 +1,16 @@
+package Java_01;
+
+public class stringreverse {
+
+	
+String s1="String Word"	;
+int size=s1.length();
+System.out.println("String Lenth: "+s1.length());
+System.out.println("Rev String: "+" "+s1+"  "+is");
+
+for(int i=0;i<size;i++)
+{
+	System.out.print(s1.charAt(s1.length()-i-1));
+}
+}
+}
